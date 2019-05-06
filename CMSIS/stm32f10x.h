bcf5e061934f8ee3e10171154f53f91dc80abb87
rise0chen <1034018425@qq.com>
2019-05-06 20:03:45 +0800

    添加fiip

--- CMSIS/stm32f10x.h
+++ CMSIS/stm32f10x.h
@@ -15,15 +15,15 @@
   *          is using in the C source code, usually in main.c. This file contains:
   *           - Configuration section that allows to select:
   *              - The device used in the target application
-  *              - To use or not the peripheral’s drivers in application code(i.e. 
-  *                code will be based on direct access to peripheral’s registers 
+  *              - To use or not the peripheralâ€™s drivers in application code(i.e. 
+  *                code will be based on direct access to peripheralâ€™s registers 
   *                rather than drivers API), this option is controlled by 
   *                "#define USE_STDPERIPH_DRIVER"
   *              - To change few application-specific parameters such as the HSE 
   *                crystal frequency
   *           - Data structures and the address mapping for all peripherals
   *           - Peripheral's registers declarations and bits definition
-  *           - Macros to access peripheral’s registers hardware
+  *           - Macros to access peripheralâ€™s registers hardware
   *
   ******************************************************************************
   * @attention
@@ -517,7 +517,7 @@ typedef __I uint32_t vuc32;  /*!< Read Only */
 typedef __I uint16_t vuc16;  /*!< Read Only */
 typedef __I uint8_t vuc8;   /*!< Read Only */
 
-//typedef enum {RESET = 0, SET = !RESET} FlagStatus, ITStatus;
+typedef enum {RESET = 0, SET = !RESET} FlagStatus, ITStatus;
 
 typedef enum {DISABLE = 0, ENABLE = !DISABLE} FunctionalState;
 #define IS_FUNCTIONAL_STATE(STATE) (((STATE) == DISABLE) || ((STATE) == ENABLE))
---
+++ fiip-link/linkUsart.h
@@ -0,0 +1,19 @@
+#ifndef __LINK_USART_H
+#define __LINK_USART_H
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "fiip-base/fiip.h"
+#include "fiip-base/link/link.h"
+#include "function.h"
+
+void startUsart(uint8_t* dev, uint32_t speed);
+
+#ifdef __cplusplus
+}
+#endif
+#endif  //__LINK_USART_H
